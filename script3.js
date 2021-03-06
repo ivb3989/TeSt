8513cea6fd5919508008d51187e1c89b2c15628b
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2021-03-06 21:31:43 +0200

    changed 'var' to 'let'

--- script3.js
+++ script3.js
@@ -1,15 +1,15 @@
 function validPairs(str) {
-    var newStr = '';
-    for (var i = 0; i < str.length; i++) {
-        for (var j = i + 1; j < str.length; j++) {
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
             if (str[i] + str[j] == '()' || str[i] + str[j] == '{}' || str[i] + str[j] == '[]') {
                 newStr = newStr + str[i] + str[j];
             }
         }
     }
-    var bracket1 = newStr.indexOf('()');
-    var bracket2 = newStr.indexOf('[]');
-    var bracket3 = newStr.indexOf('{}');
+    let bracket1 = newStr.indexOf('()');
+    let bracket2 = newStr.indexOf('[]');
+    let bracket3 = newStr.indexOf('{}');
     if (bracket1 == -1 || bracket2 == -1 || bracket3 == -1) return false;
     else return true;
 }
