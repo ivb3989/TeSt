7168dae5cbd35afef1b8401b9550788293774024
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2021-03-11 20:24:33 +0200

    changed the function

--- script3.js
+++ script3.js
@@ -1,17 +1,12 @@
 function validPairs(str) {
-    let newStr = '';
-    for (let i = 0; i < str.length; i++) {
-        for (let j = i + 1; j < str.length; j++) {
-            if (str[i] + str[j] == '()' || str[i] + str[j] == '{}' || str[i] + str[j] == '[]') {
-                newStr = newStr + str[i] + str[j];
+    if (str.includes('[') && str.includes(']')) {
+        if (str.includes('{') && str.includes('}')) {
+            if (str.includes('(') && str.includes(')')) {
+                return true;
             }
         }
     }
-    let bracket1 = newStr.indexOf('()');
-    let bracket2 = newStr.indexOf('[]');
-    let bracket3 = newStr.indexOf('{}');
-    if (bracket1 == -1 || bracket2 == -1 || bracket3 == -1) return false;
-    else return true;
+    return false;
 }
 
 console.log(validPairs('[{()}]')); //true
