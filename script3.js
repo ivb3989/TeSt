c5c2994d1d1201f5e7ad5624074cedb4c2fe0889
ivb3989 <73277168+ivb3989@users.noreply.github.com>
2021-03-15 11:50:24 +0200

    changed the function

--- script3.js
+++ script3.js
@@ -1,13 +1,34 @@
 function validPairs(str) {
-    if (str.includes('[') && str.includes(']')) {
-        if (str.includes('{') && str.includes('}')) {
-            if (str.includes('(') && str.includes(')')) {
-                return true;
+    let result = [];
+        for (const ch of str.split("")) {
+            switch (ch) {
+                case "(":
+                    result.push(1) ;
+                    break;
+                case "[":
+                    result.push(2);
+                    break;
+                case "{":
+                    result.push(3);
+                    break;
+                case "}":
+                    if (result[result.length - 1] != 3) return false;
+                    result.pop();
+                    break;
+                case "]":
+                    if (result[result.length - 1] != 2) return false;
+                    result.pop();
+                    break;
+                case ")":
+                    if (result[result.length - 1] != 1) return false;
+                    result.pop();
+                    break;
+                default:
+                    break;
             }
         }
-    }
-    return false;
+    return result.length == 0;
 }
 
-console.log(validPairs('[{()}]')); //true
-console.log(validPairs('[{)}]')); //false
+console.log(validPairs("[{()}]")); //true
+console.log(validPairs("[{)}]")); //false
